c0b73f149c551308281adc257774554d1cadcfc8
Khoa Huynh <khoawin92@gmail.com>
2018-02-08 20:52:10 -0800

    added some js to start off

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 'use strict';
+
+//creating an array to store all image objects
+Product.allProducts = [];
+
+function Product(name, filepath) {
+  this.name = name;
+  this.filepath = filepath;
+}
+
+//creating 
+
+new Product('bag', 'images/bag.jpg');
+new Product('banana', 'images/banana.jpg');
+new Product('bathroom', 'images/bathroom.jpg');
+new Product('boots', 'images/boots.jpg');
+new Product('breakfast', 'images/breakfast.jpg');
+new Product('bubblegum', 'images/bubblegum.jpg');
+new Product('chair', 'images/chair.jpg');
+new Product('cthulhu', 'images/cthulhu.jpg');
+new Product('dog-duck', 'images/dog-duck.jpg');
+new Product('dragon', 'images/dragon.jpg');
+new Product('pen', 'images/pen.jpg');
+new Product('pet-sweep', 'images/pet-sweep.jpg');
+new Product('tauntaun','images/tauntaun.jpg');
+new Product('unicorn','images/unicorn.jpg');
+new Product('usb', 'images/usb.gif');
+new Product('water-can', 'images/water-can.jpg');
+new Product('wine-glass', 'images/wine-glass');
+
+//event listener
