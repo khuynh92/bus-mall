4a59aac4195efbab68362386c797b7bc2559b53a
Khoa Huynh <khoawin92@gmail.com>
2018-02-10 13:23:07 -0800

    correctly shows number of time shown and if item was clicked

--- app.js
+++ app.js
@@ -4,48 +4,44 @@
 var allProducts = Product.allProducts = [];
 var totalClicks = 0;
 
-function Product(name, filepath, numOfClicks, numTimesShown) {
+function Product(name, filepath) {
   this.name = name;
   this.filepath = filepath;
   Product.allProducts.push(this);
   this.numOfClicks = 0;
   this.numTimesShown = 0;
-
 }
 
-new Product('bag', 'images/bag.jpg', 0, 0);
-new Product('banana', 'images/banana.jpg', 0, 0);
-new Product('bathroom', 'images/bathroom.jpg', 0, 0);
-new Product('boots', 'images/boots.jpg', 0, 0);
-new Product('breakfast', 'images/breakfast.jpg', 0, 0);
-new Product('bubblegum', 'images/bubblegum.jpg', 0, 0);
-new Product('chair', 'images/chair.jpg', 0, 0);
-new Product('cthulhu', 'images/cthulhu.jpg', 0, 0);
-new Product('dog-duck', 'images/dog-duck.jpg', 0, 0);
-new Product('dragon', 'images/dragon.jpg', 0, 0);
-new Product('pen', 'images/pen.jpg', 0, 0);
-new Product('pet-sweep', 'images/pet-sweep.jpg', 0, 0);
-new Product('tauntaun','images/tauntaun.jpg', 0, 0);
-new Product('unicorn','images/unicorn.jpg', 0, 0);
-new Product('usb', 'images/usb.gif', 0, 0);
-new Product('water-can', 'images/water-can.jpg', 0, 0);
-new Product('wine-glass', 'images/wine-glass.jpg', 0, 0);
+new Product('bag', 'images/bag.jpg');
+new Product('banana', 'images/banana.jpg');
+new Product('bathroom', 'images/bathroom.jpg');
+new Product('boots', 'images/boots.jpg');
+new Product('breakfast', 'images/breakfast.jpg');
+new Product('bubblegum', 'images/bubblegum.jpg');
+new Product('chair', 'images/chair.jpg');
+new Product('cthulhu', 'images/cthulhu.jpg');
+new Product('dog-duck', 'images/dog-duck.jpg');
+new Product('dragon', 'images/dragon.jpg');
+new Product('pen', 'images/pen.jpg');
+new Product('pet-sweep', 'images/pet-sweep.jpg');
+new Product('tauntaun', 'images/tauntaun.jpg');
+new Product('unicorn', 'images/unicorn.jpg');
+new Product('usb', 'images/usb.gif');
+new Product('water-can', 'images/water-can.jpg');
+new Product('wine-glass', 'images/wine-glass.jpg');
 
 var imgEl = [];
-var randomIndex = [.1,.2,.3]; //placeholder numbers so that correct line of code in conditional will run the first time
-var previousImages = [.4,.5,.6]; //placeholder numbers so that correct line of code in conditional will run the first time
-
+var randomIndex = [.1, .2, .3]; //placeholder numbers so that correct line of code in conditional will run the first time
+var previousImages = [.4, .5, .6]; //placeholder numbers so that correct line of code in conditional will run the first time
 
 //for loop to locate image location in HTML and creating event listener for each image
-for (var i =0; i < 3; i++) {
-  imgEl[i] = document.getElementById('image-'+ (i + 1));
+for (var i = 0; i < 3; i++) {
+  imgEl[i] = document.getElementById('image-' + (i + 1));
   imgEl[i].addEventListener('click', tallyCounter);
 }
 
 //function to create random product images that appear on HTML, created with a do while loop that checks for duplicates in same row and also checks if any of the curent iteration of images matches the previous images.
-
-function randomProduct () {
-
+function randomProduct() {
   //creating img based on name of product instance
   for (var i = 0; i < 3; i++) {
     do {
@@ -53,14 +49,16 @@ function randomProduct () {
       imgEl[i].src = Product.allProducts[randomIndex[i]].filepath;
     } while ((randomIndex[i] === previousImages[0] || randomIndex[i] === previousImages[1] || randomIndex[i] === previousImages[2]) || randomIndex[0] === randomIndex[1] || randomIndex[0] === randomIndex[2] || randomIndex[1] === randomIndex[2]);
   }
-
+  //display number of times image is shown
+  for (var m = 0; m < 3; m++) {
+    allProducts[randomIndex[m]].numTimesShown++;
+  }
   //changing id from generic to specific id
   for (var l = 0; l < 3; l++) {
     imgEl[l].setAttribute('id', Product.allProducts[randomIndex[l]].name);
   }
 
   console.log('The random number for image one is: ' + randomIndex[0] + ', The random number for image two is: ' + randomIndex[1] + ', The random number for image three is ' + randomIndex[2]);
-
   //creating an array for previous images
   for (var j = 0; j < randomIndex.length; j++) {
     previousImages[j] = randomIndex[j];
